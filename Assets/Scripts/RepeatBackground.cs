1ec97e22908d5ff6ebc8f3ff0f8018a6bbc3285f
Thom Veldpaus <thompoppins@gmail.com>
2023-10-05 10:19:20 +0100

    Reset background position after moving 50 units

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    // The start postion of the background
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set the start position to the current position at the start of the game
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < startPos.x - 50)
+        {
+            // Reset the position of the background to the start position
+            transform.position = startPos;
+        }
     }
 }
