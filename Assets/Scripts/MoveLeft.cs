b77974a1080fe44676d65a60fd5909571c068b37
Thom Veldpaus <thompoppins@gmail.com>
2023-10-05 10:44:42 +0100

    Get the PlayerController in the MoveLeft script and stop moving left if the gameOver value is true

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,16 +6,23 @@ public class MoveLeft : MonoBehaviour
 {
     // `speed` is the speed at which the object will move
     private float speed = 30;
+    // PlayerController script
+    private PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Connect with the PlayerController script
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the object to the left
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            // Move the object to the left
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
     }
 }
