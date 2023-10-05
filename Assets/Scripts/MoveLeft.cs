ccba2b3d2b9df4bf89966caef065f2ad91eeca97
Thom Veldpaus <thompoppins@gmail.com>
2023-10-05 09:49:07 +0100

    Add spawnmanager and spawn a obstacle at game start

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    // `speed` is the speed at which the object will move
     private float speed = 30;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,7 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Move the object to the left
         transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
