1b8791bbddb1bf0fec332534d30cc67f9a423b61
Thom Veldpaus <thompoppins@gmail.com>
2023-10-05 10:26:14 +0100

    Add box collider at the same size as the background and get that value inside the repeat script, repeat the background at precisely the half of the background size.

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -6,18 +6,22 @@ public class RepeatBackground : MonoBehaviour
 {
     // The start postion of the background
     private Vector3 startPos;
+    // The width the background will be repeated at
+    private float repeatWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         // Set the start position to the current position at the start of the game
         startPos = transform.position;
+        // Set the repeat width to half the width of the background
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < startPos.x - 50)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             // Reset the position of the background to the start position
             transform.position = startPos;
